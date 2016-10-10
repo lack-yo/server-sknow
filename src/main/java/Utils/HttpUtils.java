84dc460b87163b578575fec160d41f875d7b98c8
lafeloy <990507202@qq.com>
2016-10-10 13:55:45 +0800

    get请求

--- src/main/java/Utils/HttpUtils.java
+++ src/main/java/Utils/HttpUtils.java
@@ -1,8 +1,58 @@
 package Utils;
 
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.nio.charset.Charset;
+import java.util.Map;
+
 /**
  * Created by Feng.Lou on 2016/10/10.
  * 介绍一些常用的http请求方式
  */
 public class HttpUtils {
+    private static String DEFAULTENCODING = "UTF-8";
+
+    /**
+     * 该方法用来提供基本的get请求
+     * @param url 请求地址
+     * @param params 请求参数
+     * @return 返回参数以JSONObject形式
+     */
+    public static JSONObject doGet(String url , Map<String,String> params){
+        JSONObject json = new JSONObject();
+        HttpClient client = null;
+        HttpGet get = null;
+        try{
+            StringBuilder builder = new StringBuilder();
+            builder.append("?");
+            for(String key : params.keySet()){
+                builder.append(key).append("=").append(params.get(key)).append("&");
+            }
+            url += builder.toString();
+            get = new HttpGet(url);
+            client = new DefaultHttpClient();
+            HttpResponse response = client.execute(get);
+            if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+                HttpEntity entity = response.getEntity();
+                json = JSONObject.parseObject(EntityUtils.toString(entity, Charset.forName(DEFAULTENCODING)));
+                //释放io资源
+                EntityUtils.consume(entity);
+            }
+        }catch(Exception e){
+            System.out.println("[error]:"+e);
+        }finally{
+            if(client != null)
+            //释放连接资源
+            client.getConnectionManager().closeExpiredConnections();
+        }
+        return json;
+    }
+
 }
