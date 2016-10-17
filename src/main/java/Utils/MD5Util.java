6328e823c4add6c865b5a37b13a83dda5131164c
lafeloy <990507202@qq.com>
2016-10-17 13:12:40 +0800

    sign

---
+++ src/main/java/Utils/MD5Util.java
@@ -0,0 +1,96 @@
+package Utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Util {
+    /**
+     * 默认的密码字符串组合，用来将字节转换成 16 进制表示的字符,
+     */
+//    protected static char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    protected static MessageDigest messagedigest = null;
+
+    static {
+        try {
+            messagedigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException nsaex) {
+            System.err.println("MD5初始化失败，MessageDigest不支持MD5Util。");
+            nsaex.printStackTrace();
+        }
+    }
+
+    /**
+     * 生成字符串的md5校验值
+     *
+     * @param s
+     * @return
+     */
+    public static String getMD5String(String s) {
+        return getMD5String(s.getBytes());
+    }
+
+    public static String getMD5String(byte[] bytes) {
+        messagedigest.update(bytes);
+        return bufferToHex(messagedigest.digest());
+    }
+
+    private static String bufferToHex(byte bytes[]) {
+        return bufferToHex(bytes, 0, bytes.length);
+    }
+
+    private static String bufferToHex(byte bytes[], int m, int n) {
+        StringBuffer stringbuffer = new StringBuffer(2 * n);
+        int k = m + n;
+        for (int l = m; l < k; l++) {
+            appendHexPair(bytes[l], stringbuffer);
+        }
+        return stringbuffer.toString();
+    }
+
+    private static void appendHexPair(byte bt, StringBuffer stringbuffer) {
+        String c0 = hexDigits[(bt & 0xf0) >> 4];// 取字节中高 4 位的数字转换, >>>
+        // 为逻辑右移，将符号位一起右移,此处未发现两种符号有何不同
+        String c1 = hexDigits[bt & 0xf];// 取字节中低 4 位的数字转换
+        stringbuffer.append(c0);
+        stringbuffer.append(c1);
+    }
+
+    private static String byteArrayToHexString(byte b[]) {
+        StringBuffer resultSb = new StringBuffer();
+        for (int i = 0; i < b.length; i++)
+            resultSb.append(byteToHexString(b[i]));
+
+        return resultSb.toString();
+    }
+
+    private static String byteToHexString(byte b) {
+        int n = b;
+        if (n < 0)
+            n += 256;
+        int d1 = n / 16;
+        int d2 = n % 16;
+        return hexDigits[d1] + hexDigits[d2];
+    }
+
+    public static String MD5Encode(String origin, String charsetname) {
+        String resultString = null;
+        try {
+            resultString = new String(origin);
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            if (charsetname == null || "".equals(charsetname))
+                resultString = byteArrayToHexString(md.digest(resultString
+                        .getBytes()));
+            else
+                resultString = byteArrayToHexString(md.digest(resultString
+                        .getBytes(charsetname)));
+        } catch (Exception exception) {
+        }
+        return resultString;
+    }
+
+    private static final String hexDigits[] = { "0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+
+
+}
--- src/main/java/Utils/SignUtil.java
+++ src/main/java/Utils/SignUtil.java
@@ -1,8 +1,58 @@
 package Utils;
 
+import org.junit.Test;
+
+import java.security.MessageDigest;
+import java.util.*;
+
 /**
  * Created by Feng.Lou on 2016/10/12.
  * 加密验签
  */
 public class SignUtil {
+    /**
+     * 例子
+     * 将参与验签参数生产一个签名，方式MD5
+     */
+    public static boolean getSign(Map<String,String> param){
+        //剔除不参与验签字段
+        if(param.get("key")!=null){
+            param.remove("key");
+        }
+        TreeSet<String> set = new TreeSet<>();
+        StringBuilder builder = new StringBuilder();
+        //所有字段按字典序重排
+        set.addAll(param.keySet());
+
+        String paramSign = "";
+        for(String key:set){
+            if("sign".equals(key)){
+                paramSign = param.get(key);
+                continue;
+            }
+            builder.append(key).append("=").append(param.get(key)).append("&");
+        }
+
+        //去除最后一位&符号
+        builder.setLength(builder.length()-1);
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("sign");
+        //加密key
+        String secretKey = resourceBundle.getString("sign");
+        String sign = MD5Util.getMD5String(builder.toString()+secretKey);
+        System.out.println("sign:"+sign);
+    return paramSign.equals(sign);
+    }
+
+    @Test
+    public void testSign(){
+        Map<String,String> param = new HashMap<>();
+        param.put("sign","null");
+        param.put("4key","value4");
+        param.put("2key","value2");
+        param.put("3key","value3");
+        param.put("1key","value1");
+        boolean isEq = getSign(param);
+        System.out.println("sign is "+isEq);
+    }
+
 }
