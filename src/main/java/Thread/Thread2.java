b07af3b7419fc74511fb7f3eaad04ccf358599ce
lafeloy <990507202@qq.com>
2016-10-18 11:13:51 +0800

    commit

--- src/main/java/Thread/Thread2.java
+++ src/main/java/Thread/Thread2.java
@@ -1,7 +1,40 @@
 package Thread;
 
+import org.junit.Test;
+
+import java.util.concurrent.*;
+
 /**
  * Created by Feng.Lou on 2016/10/18.
+ * 线程池
  */
 public class Thread2 {
+    private ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    @Test
+    public void testPoolExecute(){
+        pool.execute(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
+
+    /**
+     * 有返回值的线程执行
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Test
+    public void testPoolSubmit() throws ExecutionException, InterruptedException {
+        Future b = pool.submit(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return true;
+            }
+        });
+        System.out.println(b.get());
+    }
+
 }
