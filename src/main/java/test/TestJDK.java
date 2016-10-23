5c1dc426c38d711bd8d5709ae65f08b978caee70
lafeloy <990507202@qq.com>
2016-10-23 21:55:02 +0800

    change commit

---
+++ src/main/java/test/TestJDK.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.Test;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by Administrator on 2016/10/20.
+ */
+public class TestJDK {
+
+ @Test
+    public void testMap(){
+     ConcurrentHashMap synMap = new ConcurrentHashMap();
+     Object o = synMap.putIfAbsent("key",new Object());
+ }
+}
