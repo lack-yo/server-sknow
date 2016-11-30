2b0f1cf8284768b9ade47c48f943de25fd730319
lafeloy <990507202@qq.com>
2016-11-30 18:29:06 +0800

    New changelist

--- src/main/java/Thread/CountDownThread.java
+++ src/main/java/Thread/CountDownThread.java
@@ -1,12 +1,16 @@
 package Thread;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by Feng.Lou on 2016/11/30.
  * 同步计数
  */
-public class CountDownThread extends Thread{
+public class CountDownThread extends Thread {
+    private static final Logger logger = LoggerFactory.getLogger(CountDownThread.class);
     private CountDownLatch countDownLatch;//同步计数器初始化
     private String name;
 
@@ -18,10 +22,10 @@ public class CountDownThread extends Thread{
     @Override
     public void run() {
         try {
-            System.out.println("============执行，"+name);
+            logger.debug("============执行，" + name);
             countDownLatch.await();
             sleep(1000);
-            System.out.println("============执行完毕，"+name);
+            logger.debug("============执行完毕，" + name);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/main/java/Thread/SemaphoreThread.java
+++ src/main/java/Thread/SemaphoreThread.java
@@ -1,8 +1,8 @@
 package Thread;
 
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.Semaphore;
 
@@ -12,7 +12,7 @@ import java.util.concurrent.Semaphore;
  * 获取信号量线程
  */
 public class SemaphoreThread extends Thread {
-    private static final Logger logger = LogManager.getLogger(SemaphoreThread.class);
+    private static final Logger logger = LoggerFactory.getLogger(SemaphoreThread.class);
     private Semaphore semaphore;//初始化信号量
     private String owner;//线程拥有者
 
@@ -25,15 +25,15 @@ public class SemaphoreThread extends Thread {
     public void run() {
 
         try {
-            System.out.println("等待获取信号，" + owner);
+            logger.debug("等待获取信号，" + owner);
             semaphore.acquire();
-            System.out.println("执行任务，" + owner);
+            logger.debug("执行任务，" + owner);
             sleep(1000);
-            System.out.println("完成任务，" + owner);
+            logger.debug("完成任务，" + owner);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("释放信号，" + owner);
+        logger.debug("释放信号，" + owner);
         semaphore.release();
 
     }
