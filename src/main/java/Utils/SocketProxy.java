13f2e707c3244a2b3fc3f49e952105fe7ddf260a
lafeloy <990507202@qq.com>
2017-02-25 13:03:55 +0800

    define socket method

--- src/main/java/Utils/SocketProxy.java
+++ src/main/java/Utils/SocketProxy.java
@@ -1,10 +1,22 @@
 package Utils;
 
+import java.net.Socket;
+
 /**
  * Created by Administrator on 2016/10/28.
  * socket协议的相关实现，网络io
  * tcp/ip 三次握手四次释放的过程
  */
-public class Socket {
+public class SocketProxy {
     //accept,connect
+    private Socket socket = new Socket();
+
+    public void connect() {
+    }
+
+    public void accept() {
+
+    }
+
+
 }
